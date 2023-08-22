103aaa98b098a1defc99d8aacd2c5c69df13a038
Niltonguerra <niltondg.39@gmail.com>
2023-08-21 22:00:30 -0300

    correção de código

--- contas.test.js
+++ contas.test.js
@@ -4,3 +4,17 @@ const contas = require('./contas');
 test("Soma 2+5 esperado 7 como resultado",()=>{
     expect(contas.soma(2,5)).toBe(7)
 })
+
+
+test("subtracao 2-5 esperado 3 como resultado",()=>{
+    expect(contas.subtracao(5,2)).toBe(3)
+})
+
+test("multiplicacao 2*5 esperado 10 como resultado",()=>{
+    expect(contas.multiplicacao(2,5)).toBe(10)
+})
+
+
+test("divisao 10/2 esperado 5 como resultado",()=>{
+    expect(contas.divisao(10,2)).toBe(5)
+})
